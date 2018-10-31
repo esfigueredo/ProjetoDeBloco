8d7e2666a0161a0f78bbf16ba2573029add95448
Ramom Costa <rokst@hotmail.com>
2018-10-30 21:07:15 -0300

    Criado tela de produto para validação(Produto).

--- app/src/main/java/com/example/esfig/projetodebloco/activities/Activity_Produto.java
+++ app/src/main/java/com/example/esfig/projetodebloco/activities/Activity_Produto.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 
 import com.example.esfig.projetodebloco.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_Produto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_produto);
     }
-
-    }
+}
