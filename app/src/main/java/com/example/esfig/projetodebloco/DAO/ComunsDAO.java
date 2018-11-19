0468ac8488b7ecd66ff94f3cf54f079483d0c634
pch91 <pch91@bol.com.br>
2018-11-18 23:16:30 -0200

    reparo no dao

--- app/src/main/java/com/example/esfig/projetodebloco/DAO/ComunsDAO.java
+++ app/src/main/java/com/example/esfig/projetodebloco/DAO/ComunsDAO.java
@@ -2,9 +2,11 @@ package com.example.esfig.projetodebloco.DAO;
 
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.esfig.projetodebloco.Util.FireBaseCalback;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseException;
@@ -33,38 +35,77 @@ public class ComunsDAO {
         }
     }
 
-    public <T> void getObject(final Class<T> oclass, final String UsertID, final String key,final FireBaseCalback fireBaseCalback)
+    public <T> void setChildEventiListener(final Class<T> oclass, final String UsertID, final String key
+                                            ,final FireBaseCalback fireBaseCalbackAdd,final FireBaseCalback fireBaseCalbackEdit
+                                            ,final FireBaseCalback fireBaseCalbackRemove,final FireBaseCalback fireBaseCalbackMoved)
             throws IllegalAccessException, InstantiationException {
-        final List<T> list = new ArrayList<T>();
 
-        //DatabaseReference thisref = Refdatabase.child(oclass.getSimpleName().toLowerCase()+"/UsertID");
-        Refdatabase.addValueEventListener(new ValueEventListener() {
+        Refdatabase.child(oclass.getSimpleName().toLowerCase() + "/" + UsertID).addChildEventListener(new ChildEventListener() {
+
+
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalbackAdd);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalbackEdit);
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalbackRemove);
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalbackMoved);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public <T> void setEventiListener(final Class<T> oclass, final String UsertID, final String key,final FireBaseCalback fireBaseCalback)
+            throws IllegalAccessException, InstantiationException {
+
+
+        Refdatabase.child(oclass.getSimpleName().toLowerCase() + "/" + UsertID).addValueEventListener(new ValueEventListener() {
 
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalback);
+            }
 
-                DataSnapshot el = dataSnapshot.child(oclass.getSimpleName().toLowerCase() + "/" + UsertID);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                for (DataSnapshot childSnapshot: el.getChildren()) {
-                    obj = childSnapshot.getValue(oclass);
-                    try {
-                        Method methodmethod = oclass.getMethod("getId",null);
-                        try {
-                            if(key.isEmpty() || ((String)methodmethod.invoke(obj)).equals(key)) {
-                                list.add(oclass.cast(obj));
-                            }
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        } catch (InvocationTargetException e) {
-                            e.printStackTrace();
-                        }
-                    } catch (NoSuchMethodException e) {
-                        e.printStackTrace();
-                    }
-                }
+            }
+        });
+    }
 
-                fireBaseCalback.onCalback(list);
+    public <T> void getObject(final Class<T> oclass, final String UsertID, final String key,final FireBaseCalback fireBaseCalback)
+            throws IllegalAccessException, InstantiationException {
+
+        //DatabaseReference thisref = Refdatabase.child(oclass.getSimpleName().toLowerCase()+"/UsertID");
+        Refdatabase.child(oclass.getSimpleName().toLowerCase() + "/" + UsertID).addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<T> list = new ArrayList<T>();
+                doevent(dataSnapshot,list,oclass,UsertID,key,fireBaseCalback);
             }
 
             @Override
@@ -72,6 +113,31 @@ public class ComunsDAO {
                 Log.d("erro Database",databaseError.getMessage());
             }
         });
+    }
+
+    private <T> void doevent(DataSnapshot dataSnapshot,List<T> list,Class<T> oclass, String UsertID,
+                             String key,FireBaseCalback fireBaseCalback){
+
+        DataSnapshot el = dataSnapshot;
+
+        for (DataSnapshot childSnapshot: el.getChildren()) {
+            obj = childSnapshot.getValue(oclass);
+            try {
+                Method methodmethod = oclass.getMethod("getId",null);
+                try {
+                    if(key.isEmpty() || ((String)methodmethod.invoke(obj)).equals(key)) {
+                        list.add(oclass.cast(obj));
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+        fireBaseCalback.onCalback(list);
     }
 
 }
