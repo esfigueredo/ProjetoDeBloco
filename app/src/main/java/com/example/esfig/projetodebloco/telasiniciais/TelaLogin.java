fa3c09c64a53e01fe53cc73701201da2767d3e9a
esfigueredo <dukarino@gmail.com>
2018-11-26 14:33:50 -0200

    atualização da activity facebook e google

--- app/src/main/java/com/example/esfig/projetodebloco/telasiniciais/TelaLogin.java
+++ app/src/main/java/com/example/esfig/projetodebloco/telasiniciais/TelaLogin.java
@@ -10,9 +10,10 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.esfig.projetodebloco.R;
-//import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-//import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import android.app.ProgressDialog;
@@ -26,78 +27,80 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class TelaLogin extends BaseActivity implements View.OnClickListener{
-    @Override
-    public void onClick(View v) {
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-/*
     private static final String TAG = "GoogleActivity";
     private static final int RC_SIGN_IN = 9001;
-    private FirebaseAuth mAuth;
-    // não sei o poerque GoogleSignInClient não importa!!!
-    //private GoogleSignInClient mGoogleSignInClient;
 
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private GoogleSignInClient mGoogleSignInClient;
     private TextView mStatusTextView;
     private TextView mDetailTextView;
-
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+
+        // Views
+        mStatusTextView = findViewById(R.id.status);
+        mDetailTextView = findViewById(R.id.detail);
+
+        // Button listeners
+        findViewById(R.id.signInButton).setOnClickListener(this);
+        //findViewById(R.id.signOutButton).setOnClickListener(this);
+       // findViewById(R.id.disconnectButton).setOnClickListener(this);
+
+        // [START config_signin]
+        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         // [END config_signin]
 
-        //mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         // [START initialize_auth]
-        // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         // [END initialize_auth]
-
-
-
-
     }
+
+    // [START on_start_check_user]
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-       // updateUI(currentUser);
+        updateUI(currentUser);
     }
+    // [END on_start_check_user]
+
+    // [START onactivityresult]
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // não sei o porque o "GoogleSignIn" não funciona!!!
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-          //  Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 // Google Sign In was successful, authenticate with Firebase
-            //    GoogleSignInAccount account = task.getResult(ApiException.class);
-             //   firebaseAuthWithGoogle(account);
-          //  } catch (ApiException e) {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-          //      Log.w(TAG, "Google sign in failed", e);
+                Log.w(TAG, "Google sign in failed", e);
                 // [START_EXCLUDE]
-          //      updateUI(null);
+                updateUI(null);
                 // [END_EXCLUDE]
-         //   }
+            }
         }
     }
+    // [END onactivityresult]
+
+    // [START auth_with_google]
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
         // [START_EXCLUDE silent]
@@ -113,12 +116,12 @@ public class TelaLogin extends BaseActivity implements View.OnClickListener{
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                          //  updateUI(user);
+                            updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Snackbar.make(findViewById(R.id.Tela_login), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
-                          //  updateUI(null);
+                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
                         }
 
                         // [START_EXCLUDE]
@@ -130,66 +133,66 @@ public class TelaLogin extends BaseActivity implements View.OnClickListener{
     // [END auth_with_google]
 
     // [START signin]
-   // private void signIn() {
-     //   Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-      //  startActivityForResult(signInIntent, RC_SIGN_IN);
-  //  }
-   // private void signOut() {
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signin]
+
+    private void signOut() {
         // Firebase sign out
         mAuth.signOut();
 
         // Google sign out
-      //  mGoogleSignInClient.signOut().addOnCompleteListener(this,
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
                 new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                    //    updateUI(null);
-                    }
-              //  });
-    //}
-    ///private void revokeAccess() {
-        // Firebase sign out
-      //  mAuth.signOut();
-
-        // Google revoke access
-      //  mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
-             //   new OnCompleteListener<Void>() {
-             ////       @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                //        updateUI(null);
+                        updateUI(null);
                     }
                 });
     }
-// esses campos não foram criados ainda, mas mesmo assim não funcionariam
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
     private void updateUI(FirebaseUser user) {
         hideProgressDialog();
         if (user != null) {
-          //  mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-          //  mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
 
-          //  findViewById(R.id.signInButton).setVisibility(View.GONE);
-           // findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+            findViewById(R.id.signInButton).setVisibility(View.GONE);
+            //findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
         } else {
-           // mStatusTextView.setText(R.string.signed_out);
+            mStatusTextView.setText(R.string.signed_out);
             mDetailTextView.setText(null);
 
-            //findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
-          //  findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
+            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
+            //findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
         }
     }
 
-//objetos não criado ainda
     @Override
     public void onClick(View v) {
         int i = v.getId();
-        /*if (i == R.id.signInButton) {
+        if (i == R.id.signInButton) {
             signIn();
         } else if (i == R.id.signOutButton) {
             signOut();
         } else if (i == R.id.disconnectButton) {
             revokeAccess();
         }
-
-
-    }*/
+    }
 }
