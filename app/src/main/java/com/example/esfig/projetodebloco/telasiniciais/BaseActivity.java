2974cced0659372f5bb5c05ca968cceab48d5bd9
Ramom Costa <rokst@hotmail.com>
2018-12-06 03:19:48 -0200

    Alterado para versão 27, Tela principal alterada para teste.

--- app/src/main/java/com/example/esfig/projetodebloco/telasiniciais/BaseActivity.java
+++ app/src/main/java/com/example/esfig/projetodebloco/telasiniciais/BaseActivity.java
@@ -2,14 +2,40 @@ package com.example.esfig.projetodebloco.telasiniciais;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.example.esfig.projetodebloco.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.common.util.VisibleForTesting;
 
+
+
 public class BaseActivity extends AppCompatActivity {
+
+// Monetização utilizando Banner , na View = " promo_list_view_houder" //
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.promo_list_item_viewhouder);
+
+        MobileAds.initialize(this,"ca-app-pub-3940256099942544~3347511713");
+        //(this, "ca-app-pub-3940256099942544~3347511713");
+        AdView mAdView=findViewById(R.id.adView);
+        AdRequest adRequest=new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        AdView adView=new AdView(this);
+        adView.setAdSize(AdSize.LARGE_BANNER);
+
+    }
+
+
 
     @VisibleForTesting
     public ProgressDialog mProgressDialog;
--- app/src/main/java/com/example/esfig/projetodebloco/viewholder/PromocaoListViewHouder.java
+++ app/src/main/java/com/example/esfig/projetodebloco/viewholder/PromocaoListViewHouder.java
@@ -1,12 +1,22 @@
 package com.example.esfig.projetodebloco.viewholder;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.esfig.projetodebloco.R;
+import com.google.firebase.database.Exclude;
 import com.xwray.groupie.ViewHolder;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
 
 public class PromocaoListViewHouder extends ViewHolder {
 
@@ -20,12 +30,18 @@ public class PromocaoListViewHouder extends ViewHolder {
     public PromocaoListViewHouder(@NonNull View rootView) {
         super(rootView);
 
-        produto= itemView.findViewById(R.id.textRcprodut);
-        marca  = itemView.findViewById(R.id.textRcmarcas);
+        produto = itemView.findViewById(R.id.textRcprodut);
+        marca = itemView.findViewById(R.id.textRcmarcas);
         local = itemView.findViewById(R.id.textRcLocal);
         preco = itemView.findViewById(R.id.textRcPreco);
         lnt = itemView.findViewById(R.id.lnint);
 
     }
 
+
+
 }
+
+
+ //mpublic class PromocaoListViewHouder extends AppCompatActivity {
+
